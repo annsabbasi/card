17ccf394a7bebb18024d5ef05e6a903fe0bb3958
annsabbasi <annsabbasi54@gmail.com>
2024-03-20 11:43:54 +0500

    Minor-Error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Main from './Components/Main';
 import Navbar from './Components/Navbar';
-import Test from './Components/Test';
-import TheTest from './Components/TheTest';
 
 function App() {
   return (
