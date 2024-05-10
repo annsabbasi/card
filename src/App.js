416cb2f81a41b1d6741f70f6f72573a141fa11a9
annsabbasi <annsabbasi54@gmail.com>
2024-05-10 21:33:44 +0500

    Routing added in Dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './Pages/Dashboard';
-import BookAd from './Components/BookAd';
-import GenerateCardChild from './Components/GenerateCardChild';
-import GenerateCards from './Components/GenerateCards';
-import MyCards from './Components/MyCards';
-import SubPlan from './Components/SubPlan';
 import Home from './Pages/Home';
+import { BookAd, GenerateCardChild, GenerateCards, MyCards, SubPlan } from './Components/CmptIndex'
+
 function App() {
   return (
     <>
       {/* <Dashboard /> */}
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/Dashboard' element={<Dashboard />} />
-        <Route path='/BookAd' element={<BookAd />} />
-        <Route path='/GenerateCardsChild' element={<GenerateCardChild />} />
-        <Route path='/GenerateCards' element={<GenerateCards />} />
-        <Route path='/MyCards' element={<MyCards />} />
-        <Route path='/Subplan' element={<SubPlan />} />
+        <Route path='/Dashboard' element={<Dashboard />}>
+          <Route path='BookAd' element={<BookAd />} />
+          <Route path='GenerateCardsChild' element={<GenerateCardChild />} />
+          <Route path='GenerateCards' element={<GenerateCards />} />
+          <Route path='MyCards' element={<MyCards />} />
+          <Route path='Subplan' element={<SubPlan />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ src/Components/CmptIndex.js
@@ -0,0 +1,5 @@
+export { default as BookAd } from './BookAd'
+export { default as GenerateCardChild } from './GenerateCardChild'
+export { default as GenerateCards } from './GenerateCards'
+export { default as MyCards } from './MyCards'
+export { default as SubPlan } from './SubPlan'
